a6704d24722c9d6e03fffb9035434c34c6cb6576
AtsushiSakai <asakai.amsl+github@gmail.com>
2015-12-26 13:52:37 +0900

    add

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# SimpleTkGUIKit
+SimpleTkGUIKit
+
+===============
 Simple GUI kit with Python tkinter
+
+# Install
+
+# Usage
+
+## GetFilePathsWithDialog
+    fileList=SimpleTkGUIKit.GetFilePathsWithDialog()
+    print fileList
+
+    fileList=SimpleTkGUIKit.GetFilePathsWithDialog(fileTypes=[('CSV Files', '.csv')])
+    print fileList
+
+
+## GetRadioButtonSelect
+    (selected,index)=SimpleTkGUIKit.GetRadioButtonSelect(["sample a","sample b","sample c"])
+
+    print selected
+    (selected,index)=SimpleTkGUIKit.GetRadioButtonSelect(["Red","Green","Blue"],title="Color Select",msg="Please select color")
+    print selected
+
+
+## GetListSelect
+    print "GetListSelect"
+    (selected,index)=SimpleTkGUIKit.GetListSelect(["sample a","sample b","sample c"])
+    print (selected,index)
+
+    (selected,index)=SimpleTkGUIKit.GetListSelect(["sample a","sample b","sample c"], title="Select sample", msg="Please select sample")
+    print (selected,index)
+
+
+## GetRadioButtonSelect
+    print "GetCheckButtonSelect"
+    optList=SimpleTkGUIKit.GetCheckButtonSelect(["sample a","sample b","sample c"], title="Select sample", msg="Please select sample")
+
+
+## GetEntries
+    print "GetEntries"
+    dataList=SimpleTkGUIKit.GetEntries(["x","y","z"], title="set entris", msg="Please set entries")
+    print dataList
+
+
--- SimpleTkGUIKit/SimpleTkGUIKit.py
+++ SimpleTkGUIKit/SimpleTkGUIKit.py
@@ -170,35 +170,30 @@ class SimpleTkGUIKit:
 
 if __name__ == '__main__':
     #Test
-    #  print "GetFilePathsWithDialog"
-    #  fileList=SimpleTkGUIKit.GetFilePathsWithDialog()
-    #  print fileList
-    #  fileList=SimpleTkGUIKit.GetFilePathsWithDialog(fileTypes=[('CSV Files', '.csv')])
-    #  print fileList
 
-    #  print "GetRadioButtonSelect"
-    #  (selected,index)=SimpleTkGUIKit.GetRadioButtonSelect(["sample a","sample b","sample c"])
-    #  print selected
-    #  (selected,index)=SimpleTkGUIKit.GetRadioButtonSelect(["Red","Green","Blue"],title="Color Select",msg="Please select color")
-    #  print selected
+    print "GetFilePathsWithDialog"
+    fileList=SimpleTkGUIKit.GetFilePathsWithDialog()
+    print fileList
+    fileList=SimpleTkGUIKit.GetFilePathsWithDialog(fileTypes=[('CSV Files', '.csv')])
+    print fileList
 
-    #  print "GetListSelect"
-    #  (selected,index)=SimpleTkGUIKit.GetListSelect(["sample a","sample b","sample c"])
-    #  print (selected,index)
-    #  (selected,index)=SimpleTkGUIKit.GetListSelect(["sample a","sample b","sample c"], title="Select sample", msg="Please select sample")
-    #  print (selected,index)
+    print "GetRadioButtonSelect"
+    (selected,index)=SimpleTkGUIKit.GetRadioButtonSelect(["sample a","sample b","sample c"])
+    print selected
+    (selected,index)=SimpleTkGUIKit.GetRadioButtonSelect(["Red","Green","Blue"],title="Color Select",msg="Please select color")
+    print selected
 
-    #  print "GetCheckButtonSelect"
-    #  optList=SimpleTkGUIKit.GetCheckButtonSelect(["sample a","sample b","sample c"], title="Select sample", msg="Please select sample")
+    print "GetListSelect"
+    (selected,index)=SimpleTkGUIKit.GetListSelect(["sample a","sample b","sample c"])
+    print (selected,index)
+    (selected,index)=SimpleTkGUIKit.GetListSelect(["sample a","sample b","sample c"], title="Select sample", msg="Please select sample")
+    print (selected,index)
 
-    #  print "GetEntries"
-    #  dataList=SimpleTkGUIKit.GetEntries(["x","y","z"], title="set entris", msg="Please set entries")
-    #  print dataList
+    print "GetCheckButtonSelect"
+    optList=SimpleTkGUIKit.GetCheckButtonSelect(["sample a","sample b","sample c"], title="Select sample", msg="Please select sample")
 
-
-
-
-
-
+    print "GetEntries"
+    dataList=SimpleTkGUIKit.GetEntries(["x","y","z"], title="set entris", msg="Please set entries")
+    print dataList
 
 
