788bf891899b9e921e14213db3c354c68a8e1c8a
Atsushi Sakai <asakai.amsl+github@gmail.com>
2017-09-03 16:35:38 -0700

    fix for python3

--- SimpleTkGUIKit/SimpleTkGUIKit.py
+++ SimpleTkGUIKit/SimpleTkGUIKit.py
@@ -1,36 +1,38 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
-#
-#   brief:  Simple Tkinter GUI kit library 
+
+#   brief:  Simple tkinter GUI kit library
 #   author: Atsushi Sakai
 #   Copyright (c): 2015 Atsushi Sakai
 #
-import Tkinter
-import tkFileDialog
+import tkinter
+from tkinter import filedialog
+
 
 class SimpleTkGUIKit:
-       
+
     @classmethod
-    def GetFilePathsWithDialog(self,fileTypes=[]):
+    def GetFilePathsWithDialog(self, fileTypes=[]):
         """
             Multipul File Select with dialog
 
             fileTypes: you can choise file extension
                 ex) fileTypes=[('Excel Files', '.xlsx')]
         """
-        root=Tkinter.Tk()
+        root = tkinter.Tk()
         root.withdraw()
-        filepath = tkFileDialog.askopenfilenames(filetypes = fileTypes, parent=root)
+        filepath = filedialog.askopenfilenames(
+            filetypes=fileTypes, parent=root)
 
-        if isinstance(filepath,str):
-            fileList=filepath.split(" ")
-        elif isinstance(filepath,tuple):
-            fileList=list(filepath)
-        elif isinstance(filepath,list):
-            fileList=filepath
+        if isinstance(filepath, str):
+            fileList = filepath.split(" ")
+        elif isinstance(filepath, tuple):
+            fileList = list(filepath)
+        elif isinstance(filepath, list):
+            fileList = filepath
 
         root.destroy()
-        print str(len(fileList))+" files are selected"
+        print(str(len(fileList)) + " files are selected")
 
         return fileList
 
@@ -44,30 +46,31 @@ class SimpleTkGUIKit:
 
         return (seldctedItem, selectedindex)
         """
-        root = Tkinter.Tk()
+        root = tkinter.Tk()
         root.title(title)
-        self.val = Tkinter.IntVar()
+        self.val = tkinter.IntVar()
         self.val.set(0)
 
-        if msg!="":
-            Tkinter.Label(root, text=msg).pack()
+        if msg != "":
+            tkinter.Label(root, text=msg).pack()
 
-        index=0
+        index = 0
         for item in selectList:
-            Tkinter.Radiobutton(root, text=item, variable=self.val, value=index).pack(anchor=Tkinter.W)
-            index+=1
+            tkinter.Radiobutton(root, text=item, variable=self.val,
+                                value=index).pack(anchor=tkinter.W)
+            index += 1
 
-        Tkinter.Button(root, text="OK", fg="black", command=root.quit).pack()
+        tkinter.Button(root, text="OK", fg="black", command=root.quit).pack()
         root.mainloop()
         root.destroy()
 
-        print selectList[self.val.get()]+" is selected"
+        print(selectList[self.val.get()] + " is selected")
         return (selectList[self.val.get()], self.val.get())
 
     @classmethod
     def GetListSelect(self, selectList, title="Select", msg=""):
         """
-        Create list with selectList, 
+        Create list with selectList,
         and then return seleced string and index
 
         title: Window name
@@ -76,24 +79,24 @@ class SimpleTkGUIKit:
         return (seldctedItem, selectedindex)
 
         """
-        root = Tkinter.Tk()
+        root = tkinter.Tk()
         root.title(title)
 
-        label = Tkinter.Label(root,text=msg)
+        label = tkinter.Label(root, text=msg)
         label.pack()
 
-        listbox = Tkinter.Listbox(root)
+        listbox = tkinter.Listbox(root)
         for i in selectList:
-            listbox.insert(Tkinter.END, i)
+            listbox.insert(tkinter.END, i)
         listbox.pack()
 
-        Tkinter.Button(root, text="OK", fg="black", command=root.quit).pack()
+        tkinter.Button(root, text="OK", fg="black", command=root.quit).pack()
         root.mainloop()
 
-        selected=listbox.get(listbox.curselection())
-        print selected+" is selected" 
+        selected = listbox.get(listbox.curselection())
+        print(selected + " is selected")
         root.destroy()
-        return (selected,selectList.index(selected))
+        return (selected, selectList.index(selected))
 
     @classmethod
     def GetCheckButtonSelect(self, selectList, title="Select", msg=""):
@@ -106,28 +109,28 @@ class SimpleTkGUIKit:
         return selected dictionary
             {'sample b': False, 'sample c': False, 'sample a': False}
         """
-        root = Tkinter.Tk()
+        root = tkinter.Tk()
         root.title(title)
 
-        label = Tkinter.Label(root,text=msg)
+        label = tkinter.Label(root, text=msg)
         label.pack()
 
-        optList=[]
+        optList = []
         for item in selectList:
-            opt = Tkinter.BooleanVar()
+            opt = tkinter.BooleanVar()
             opt.set(False)
-            Tkinter.Checkbutton(root, text = item, variable =opt).pack()
+            tkinter.Checkbutton(root, text=item, variable=opt).pack()
             optList.append(opt)
 
-        Tkinter.Button(root, text="OK", fg="black", command=root.quit).pack()
+        tkinter.Button(root, text="OK", fg="black", command=root.quit).pack()
         root.mainloop()
         root.destroy()
 
-        result={}
-        for (opt, select) in zip(optList,selectList):
-            result[select]=opt.get()
+        result = {}
+        for (opt, select) in zip(optList, selectList):
+            result[select] = opt.get()
 
-        print result
+        print(result)
         return result
 
     @classmethod
@@ -141,59 +144,32 @@ class SimpleTkGUIKit:
         return data dictionary like:
         {'y': '5.0', 'x': '100', 'z': 'save'}
         """
-        root = Tkinter.Tk()
+        root = tkinter.Tk()
         root.title(title)
 
-        label = Tkinter.Label(root,text=msg)
+        label = tkinter.Label(root, text=msg)
         label.pack()
 
-        entries=[]
+        entries = []
         for item in dataList:
-            Tkinter.Label(root,text=item).pack()
-            entry=Tkinter.Entry(root)
+            tkinter.Label(root, text=item).pack()
+            entry = tkinter.Entry(root)
             entry.pack()
             entries.append(entry)
 
-        #print entries
-        Tkinter.Button(root, text="OK", fg="black", command=root.quit).pack()
+        # print entries
+        tkinter.Button(root, text="OK", fg="black", command=root.quit).pack()
         root.mainloop()
 
-        result={}
-        for (entry, data) in zip(entries,dataList):
-            result[data]=entry.get()
+        result = {}
+        for (entry, data) in zip(entries, dataList):
+            result[data] = entry.get()
 
         root.destroy()
-        print result
+        print(result)
         return result
 
 
-
 if __name__ == '__main__':
-    #Test
-
-    print "GetFilePathsWithDialog"
-    fileList=SimpleTkGUIKit.GetFilePathsWithDialog()
-    print fileList
-    fileList=SimpleTkGUIKit.GetFilePathsWithDialog(fileTypes=[('CSV Files', '.csv')])
-    print fileList
-
-    print "GetRadioButtonSelect"
-    (selected,index)=SimpleTkGUIKit.GetRadioButtonSelect(["sample a","sample b","sample c"])
-    print selected
-    (selected,index)=SimpleTkGUIKit.GetRadioButtonSelect(["Red","Green","Blue"],title="Color Select",msg="Please select color")
-    print selected
-
-    print "GetListSelect"
-    (selected,index)=SimpleTkGUIKit.GetListSelect(["sample a","sample b","sample c"])
-    print (selected,index)
-    (selected,index)=SimpleTkGUIKit.GetListSelect(["sample a","sample b","sample c"], title="Select sample", msg="Please select sample")
-    print (selected,index)
-
-    print "GetCheckButtonSelect"
-    optList=SimpleTkGUIKit.GetCheckButtonSelect(["sample a","sample b","sample c"], title="Select sample", msg="Please select sample")
-
-    print "GetEntries"
-    dataList=SimpleTkGUIKit.GetEntries(["x","y","z"], title="set entris", msg="Please set entries")
-    print dataList
-
-
+    # Test
+    pass
